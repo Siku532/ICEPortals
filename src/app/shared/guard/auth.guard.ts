01457525c0ba44c4d6a482f461c82bbbfadf518a
Sikandar Niaz <sikandar.niaz@marksman.com.pk>
2020-12-09 18:24:33 +0500

    shop page visible without login

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,22 +1,25 @@
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { CanActivate } from "@angular/router";
+import { Router } from "@angular/router";
 import * as moment from "moment";
+import { Location } from "@angular/common";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router) {}
+  constructor(private router: Router, private location: Location) {}
 
-    canActivate() {
-        
-        var t = moment(new Date).format('YYYY-MM-DD');
-        var st = localStorage.getItem('today');
-        // if (t > st) this.router.navigate(['/login']);
-        if (localStorage.getItem('isLoggedin') && t <= st) {
-            return true;
-        }
-
-        this.router.navigate(['/login']);
-        return false;
+  canActivate() {
+    var t = moment(new Date()).format("YYYY-MM-DD");
+    var st = localStorage.getItem("today");
+    // if (t > st) this.router.navigate(['/login']);
+    if (
+      (localStorage.getItem("isLoggedin") && t <= st) ||
+      this.location.path().indexOf("/details/") > -1
+    ) {
+      return true;
     }
+
+    this.router.navigate(["/login"]);
+    return false;
+  }
 }
